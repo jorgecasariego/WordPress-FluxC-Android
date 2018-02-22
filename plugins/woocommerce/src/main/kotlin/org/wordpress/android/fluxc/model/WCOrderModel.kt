647918a816eee74bbabc464e4d142b75b795f412
Alex Forcier <alex.forcier@automattic.com>
2018-02-22 12:30:07 -0500

    Store full billing and shipping info for orders

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCOrderModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCOrderModel.kt
@@ -16,10 +16,38 @@ data class WCOrderModel(@PrimaryKey @Column private var id: Int = 0) : Identifia
     @Column var total: Float = 0F
     @Column var billingFirstName: String = ""
     @Column var billingLastName: String = ""
+    @Column var billingCompany: String = ""
+    @Column var billingAddress1: String = ""
+    @Column var billingAddress2: String = ""
+    @Column var billingCity: String = ""
+    @Column var billingState: String = ""
+    @Column var billingPostcode: String = ""
+    @Column var billingCountry: String = ""
+    @Column var billingEmail: String = ""
+    @Column var billingPhone: String = ""
+
+    @Column var shippingFirstName: String = ""
+    @Column var shippingLastName: String = ""
+    @Column var shippingCompany: String = ""
+    @Column var shippingAddress1: String = ""
+    @Column var shippingAddress2: String = ""
+    @Column var shippingCity: String = ""
+    @Column var shippingState: String = ""
+    @Column var shippingPostcode: String = ""
+    @Column var shippingCountry: String = ""
 
     override fun getId(): Int = id
 
     override fun setId(id: Int) {
         this.id = id
     }
+
+    /**
+     * Returns true if there are shipping details defined for this order,
+     * which are different from the billing details.
+     *
+     * If no separate shipping details are defined, the billing details should be used instead,
+     * as the shippingX properties will be empty.
+     */
+    fun hasSeparateShippingDetails() = shippingCountry.isNotEmpty()
 }
